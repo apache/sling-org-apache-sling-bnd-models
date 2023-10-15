8033ca4c5b8bcf425b50cf012e672a80bfb7cf7e
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-10-15 09:48:03 +0200

    SLING-12099 update to sling-parent 52 update to latest mockito version

--- src/test/java/org/apache/sling/bnd/models/AbstractModelsScannerPluginTest.java
+++ src/test/java/org/apache/sling/bnd/models/AbstractModelsScannerPluginTest.java
@@ -35,7 +35,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import aQute.bnd.osgi.Builder;
 import aQute.bnd.osgi.Jar;
